20771edc225a9e4913f0a80fc05b53d7d8442cce
Matt Pierson <matt.pierson789@gmail.com>
2023-04-21 14:31:41 -0400

    authentication set-up

--- Reddit/app/controllers/application_controller.rb
+++ Reddit/app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
 class ApplicationController < ActionController::Base
+
+
+    helper_method :cureent_user, :logged_in?
+
+#Chr(r)LLL
+
+def current_user
+
+    @surrent_user = User.find_by(session_token: session[:session_token])
+
+end 
+
+def require_logged_in
+
+
+redirect_to new_session_url unless logged_in?
+
+
 end
+
+def require_logged_out
+
+redirect_to users_url if logged_in?
+    
+end 
+
+
+def logged_in? 
+
+    !!current_user  #no @ symbol
+
+end 
+
+def login!(user)
+
+@current_user = user                    #why do we use @current user and then sometimes not
+
+ session[:session_token] = user.reset_session_token
+ 
+end 
+
+def logout!
+
+
+current_user.reset_session_token
+
+current_user = nil 
+session_token = nil
+
+
+end 
+
---
+++ Reddit/app/controllers/sessions_controller.rb
@@ -0,0 +1,48 @@
+class SessionsController < ApplicationController
+
+    before_action require_logged_in
+
+
+    def new 
+
+        @user = User.new
+
+        render :new
+
+    end 
+
+
+    def create 
+
+        password = params[:users][:password] 
+        username = params[:users][:username]
+
+        @user = User.find_by_credentials(username, password)
+
+        if @user && @user.is_password(password)
+            login!(@user)
+            redirect_to users_url
+
+        else 
+
+            @user = User.new(username, password)   #Is this for passing the @user into the form?
+
+
+            flash.new[:errors] = ["Invalid Credentials"]
+
+            render :new
+        end 
+
+
+    end 
+
+
+    def destroy 
+
+        logout!
+
+        redirect_to new_session_url
+
+    end 
+
+end 
